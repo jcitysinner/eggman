0c4ab559048175ac2380f6f97b7bc386cf752000
John Leschinski <picard102@gmail.com>
2015-09-09 23:42:29 -0400

    Social

--- wp-content/themes/EggMan/_objects/social.php
+++ wp-content/themes/EggMan/_objects/social.php
@@ -6,44 +6,57 @@ $instagram =  cmb2_get_option('eggman_options', 'instagram');
 $hashtag =  cmb2_get_option('eggman_options', 'hashtag');
 
 ?>
-  <div class='social-section'>
-    <div class='wrapper'>
-      <div class='instagram-post social'>
+
+<section class='social'>
+  <div class='wrapper'>
+
+      <div class='social-post instagram'>
         <img class='' src="http://placehold.it/300x300">
       </div>
-      <div class='twitter-post social'>
+
+      <div class='social-post instagram'>
+        <img class='' src="http://placehold.it/300x300">
+      </div>
+
+
+
+      <div class='social-post twitter'>
         <img src="http://placehold.it/300x300">
         <p>Not joking when I say @TheEggmanCanada was the BEST meal we had while visiting Toronto.</p>
-        <svg class='twitter-icon'><use xlink:href="#twitter-icon"></use></svg>
+        <svg><use xlink:href="#twitter-icon"></use></svg>
       </div>
-      <div class='instagram-post social'>
+      
+            <div class='social-post instagram'>
         <img class='' src="http://placehold.it/300x300">
       </div>
-      <div class='instagram-post social'>
+
+
+      <div class='social-post instagram'>
         <img class='' src="http://placehold.it/300x300">
       </div>
-      <div class='instagram-post social'>
-        <img class='' src="http://placehold.it/300x300">
-      </div>
-    </div>
+
+
+
+
   </div>
 
   <div class="share">
     <div class="wrapper"> 
       <?php if (!empty($hashtag)) { echo '<span class="hashtag">#'.$hashtag.'</span>'; } ?>
       <div class="accounts">
-          <span>Follow the EggMan</span>
-          <?php
-            if (!empty($twitter)) {
-              echo '<a href="'.$twitter.'"><svg><use xlink:href="#twitter-icon"></use></svg></a>';
-            }
-            if (!empty($facebook)) {
-              echo '<a href="'.$facebook.'"><svg><use xlink:href="#facebook-icon"></use></svg></a>';
-            }
-            if (!empty($instagram)) {
-              echo '<a href="'.$instagram.'"><svg><use xlink:href="#instagram-icon"></use></svg></a>';
-            }
-          ?>
-      </div>
+        <span>Follow the EggMan</span>
+        <?php
+          if (!empty($twitter)) {
+            echo '<a href="'.$twitter.'"><svg><use xlink:href="#twitter-icon"></use></svg></a>';
+          }
+          if (!empty($facebook)) {
+            echo '<a href="'.$facebook.'"><svg><use xlink:href="#facebook-icon"></use></svg></a>';
+          }
+          if (!empty($instagram)) {
+            echo '<a href="'.$instagram.'"><svg><use xlink:href="#instagram-icon"></use></svg></a>';
+          }
+        ?>
     </div>
   </div>
+
+</section>
