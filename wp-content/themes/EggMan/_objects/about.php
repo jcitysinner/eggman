4136647b02a5c13177261818072a72f7710cdd35
John Leschinski <picard102@gmail.com>
2015-09-17 00:50:32 -0400

    About Value

--- wp-content/themes/EggMan/_objects/about.php
+++ wp-content/themes/EggMan/_objects/about.php
@@ -2,42 +2,45 @@
 <section class='about'>
 
   <div class='wrapper'>
-
-    <h2>What Makes The Eggman So Delicious?</h2>
+ 
+    <h1>What Makes The Eggman So Delicious?</h1>
     
+    <div class="grid">
       <div class='about-column'>
         <img class='' src="http://placehold.it/350x150">
-        <h3>Farm Fresh</h3>
+        <h2>Farm Fresh <svg><use xlink:href="#farm-icon"></use></svg></h2>
         <p>All of our ingredients come straight from local farms and farmers markets, making sure that our customers get the freshest breakfast in the city.</p>
       </div>
 
       <div class='about-column'>
         <img class='' src="http://placehold.it/350x150">
-        <h3>Great Location</h3>
+        <h2>Great Location <svg><use xlink:href="#location-icon"></use></svg></h2>
         <p>During the week, the EggMan truck parks in the heart of Downtown Toronto, usually around Young and Elm Street. A short walk from The Eaton Centre!</p>
       </div>
 
       <div class='about-column'>
         <img class='' src="http://placehold.it/350x150">
-        <h3>Made with Care</h3>
+        <h2>Made with Care <svg><use xlink:href="#care-icon"></use></svg></h2>
         <p>The chefs that prepare your food at The EggMan food truck are all experts at their craft who want nothing more than for you to chow down on a delicious breakfast.</p>
       </div>
+
+    </div>
 
   </div>
 
 </section>
 
 
-  <div class="team">
-    <div class="wrapper">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/img/raster/compressed/chef.png">
-        <div class="col_2">
-          <h1>Tom is The EggMan</h1>
-          <p>Bacon ipsum dolor amet jerky pork belly pancetta shoulder. Flank short loin short ribs jowl turducken brisket meatball alcatra doner tenderloin pig t-bone ball tip. Meatloaf andouille sausage pork chop fatback. </p>
-          <a href="" class="meet">Meet The EggSquad</a>
-        </div>
-    </div>
+<section class="team">
+  <div class="wrapper">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/img/raster/compressed/chef.png">
+      <div class="col_2">
+        <h1>Tom is The EggMan</h1>
+        <p>Bacon ipsum dolor amet jerky pork belly pancetta shoulder. Flank short loin short ribs jowl turducken brisket meatball alcatra doner tenderloin pig t-bone ball tip. Meatloaf andouille sausage pork chop fatback. </p>
+        <a href="" class="meet">Meet The EggSquad</a>
+      </div>
   </div>
+</section>
 
 
 
