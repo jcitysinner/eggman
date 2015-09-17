7fd5a6b8baacbcd59483d1893aca6f1cacba8cee
John Leschinski <picard102@gmail.com>
2015-09-16 23:30:25 -0400

    About Resp

--- wp-content/themes/EggMan/_objects/about.php
+++ wp-content/themes/EggMan/_objects/about.php
@@ -5,25 +5,27 @@
 
     <h2>What Makes The Eggman So Delicious?</h2>
     
-    <div class='about-wrap'>
       <div class='about-column'>
         <img class='' src="http://placehold.it/350x150">
         <h3>Farm Fresh</h3>
         <p>All of our ingredients come straight from local farms and farmers markets, making sure that our customers get the freshest breakfast in the city.</p>
       </div>
+
       <div class='about-column'>
         <img class='' src="http://placehold.it/350x150">
         <h3>Great Location</h3>
         <p>During the week, the EggMan truck parks in the heart of Downtown Toronto, usually around Young and Elm Street. A short walk from The Eaton Centre!</p>
       </div>
+
       <div class='about-column'>
         <img class='' src="http://placehold.it/350x150">
         <h3>Made with Care</h3>
         <p>The chefs that prepare your food at The EggMan food truck are all experts at their craft who want nothing more than for you to chow down on a delicious breakfast.</p>
       </div>
-    </div>
 
   </div>
+
+</section>
 
 
   <div class="team">
@@ -38,4 +40,4 @@
   </div>
 
 
-</section>
+
