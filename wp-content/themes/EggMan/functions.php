fd4d19845afa77cbc1476f13bf5bc939663d4aa4
John Leschinski <picard102@gmail.com>
2015-09-22 23:02:09 -0400

    Responsive

--- wp-content/themes/EggMan/functions.php
+++ wp-content/themes/EggMan/functions.php
@@ -264,4 +264,16 @@ function remove_wp_logo( $wp_admin_bar ) {
 $wp_admin_bar->remove_node('wp-logo');
 }
 
+
+
+
+
+function get_the_content_with_formatting ($more_link_text = '(more...)', $stripteaser = 0, $more_file = '') {
+  $content = get_the_content($more_link_text, $stripteaser, $more_file);
+  $content = apply_filters('the_content', $content);
+  $content = str_replace(']]>', ']]&gt;', $content);
+  return $content;
+}
+
+
 ?>
