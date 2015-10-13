07c6dc4c848e4927afe7dfb5e7cf4f6ccedbf58d
John Leschinski <picard102@gmail.com>
2015-10-13 09:47:03 -0400

    #1

--- wp-content/themes/EggMan/_objects/status.php
+++ wp-content/themes/EggMan/_objects/status.php
@@ -1,31 +1,16 @@
-<?php 
-$status = closed; // Soon or Closed
-?>
 
 <section class="status closed">
-
   <?php
-    if ($status == 'open' || $status == 'soon') {
       echo '<div class="status-icon">';
       include(get_template_directory() .'/img/svg/compressed/sun-icon.svg');
       echo '</div>';
-    }
   ?>
-
-  <p id='status-copy'><span>OPEN</span>MORE TEXT</p>
+  <p id='status-copy'> 
+     <span>Loading <div class="loading"><svg><use xlink:href="#egg-cross"></use></svg></div></span> 
+  </p>
 
   <div class="controls">
 
-    <?php
-      if ($status == 'open' || $status == 'soon') {
-        echo '
-        <a href="#">
-          <svg><use xlink:href="#maps-icon"></use></svg>
-          <span>View on Google Maps</span>
-        </a>';
-      }
-    ?>
-   
     <a href="#" id='schedule-trigger' class='schedule-trigger'>
       <svg class="calendar-icon"><use xlink:href="#calendar-icon"></use></svg>
       <span>See Future Schedule</span>
--- wp-content/themes/EggMan/js/main.js
+++ wp-content/themes/EggMan/js/main.js
@@ -135,21 +135,15 @@ $(".menu_target").on("click", ".close", function() {
 
   function setOpen(data) {
 
-
     var open = false;
 
     if (data.open.length) {
-
       open = true;
-
     } else {
-
       open = false;
-
     }
 
     if (open) {
-
       var currentOpen = 0;
       var currentClose = 0;
       var soon = false;
@@ -157,24 +151,19 @@ $(".menu_target").on("click", ".close", function() {
       var info;
       var months = ['Jan','Feb','March','April','May','June','July','Aug','Sept','Oct','Nov','Dec'];
 
-      console.log(currentTime);
-      console.log(data.open[0].start);
+      console.log(currentTime + ' currenttime');
+      console.log(data.open[0].start + ' Open Time');
 
       for (i = 0; i < data.open.length; i++) {
-
         if (data.open[i].start < currentTime && data.open[i].end > currentTime) {
-
           currentClose = data.open[i].end;
           info = data.open[i].display;
           soon = true;
-
         } else if (data.open[i].start > currentTime && soon == false) {
-
           currentClose = data.open[i].end;
           currentOpen = data.open[i].start;
           info = data.open[i].display;
           soon = true;
-
         }
 
         var thisDateOpen = new Date(data.open[i].start*1000);
@@ -187,7 +176,6 @@ $(".menu_target").on("click", ".close", function() {
         var thisLong = data.open[i].longitude;
 
         if (thisDateOpenHour > 12) {
-
           thisDateOpenHour = thisDateOpenHour - 12 + 'PM';
 
         } else if (thisDateOpenHour === 0) {
@@ -197,13 +185,10 @@ $(".menu_target").on("click", ".close", function() {
         } else {
 
           thisDateOpenHour = thisDateOpenHour + 'AM';
-
         }
 
-         if (thisDateCloseHour > 12) {
-
+        if (thisDateCloseHour > 12) {
           thisDateCloseHour = thisDateCloseHour - 12 + 'PM';
-
         } else if (thisDateCloseHour === 0) {
 
           thisDateCloseHour = '12AM';
@@ -214,55 +199,47 @@ $(".menu_target").on("click", ".close", function() {
 
         }
 
-        var html = $('.schedule dl').html() + '<dt>'+ thisDateMonth +' '+ thisDateDate +'</dt><dd><div><strong>'+thisDateOpenHour+' - '+thisDateCloseHour+'</strong>'+data.open[i].display+'<a href="http://maps.google.com/maps?z=18&q='+ thisLat +',' + thisLong +'""><svg><use xlink:href="#maps-icon"></use></svg><span>View on Google Maps</span></a></div></dd>';
+        var html = $('.schedule dl').html() + '<dt>'+ thisDateMonth +' '+ thisDateDate +'</dt> <dd> <div><strong>'+thisDateOpenHour+' - '+thisDateCloseHour+'</strong>'+data.open[i].display+'<a href="http://maps.google.com/maps?z=18&q='+ thisLat +',' + thisLong +'"><svg><use xlink:href="#maps-icon"></use></svg><span>View on Google Maps</span></a></div> </dd>';
 
         $('.schedule dl').html(html);
 
       }
 
+        console.log(currentOpen);
+        console.log(currentClose);
+
       if (currentClose === 0) {
 
-        $('#status-copy').html('<span>Currently Closed</span>Sorry, The Eggman Truck is closed right now!');
+        $('#status-copy').html('<span>Currently Closed</span>Sorry, we\'re is closed right now!');
 
       } else if(currentOpen !== 0){
 
-        console.log(currentOpen);
         var date = new Date(currentOpen*1000);
         var hours = date.getHours();
         var closeDate = new Date(currentClose*1000);
-        var closeHours = date.getHours();
+        var closeHours = closeDate.getHours();
 
         if (hours > 12) {
-
           hours = hours - 12 + 'PM';
-
         } else if (hours === 0) {
-
           hours = '12AM';
-
         } else {
-
           hours = hours + 'AM';
-
         }
 
         if (closeHours > 12) {
-
           closeHours = closeHours - 12 + 'PM';
-
         } else if (closeHours === 0) {
-
           closeHours = '12AM';
-
         } else {
-
           closeHours = closeHours + 'AM';
-
         }
 
         $('#status-copy').html('<span>Will be open at ' + hours + ' until ' + closeHours + '</span>' + info);
         $('.status').removeClass('closed');
         $('.status').addClass('soon');
+        $('.status .controls').prepend('<a href="http://maps.google.com/maps?z=18&q='+ thisLat +',' + thisLong +'"><svg><use xlink:href="#maps-icon"></use></svg><span>View on Google Maps</span></a>');
+        $('.status .loading').remove();
 
       } else {
 
@@ -286,13 +263,17 @@ $(".menu_target").on("click", ".close", function() {
         $('#status-copy').html('<span>Open Now Until ' + hours + '</span>' + info);
         $('.status').removeClass('closed');
         $('.status').addClass('open');
+        $('.status .controls').prepend('<a href="http://maps.google.com/maps?z=18&q='+ thisLat +',' + thisLong +'"><svg><use xlink:href="#maps-icon"></use></svg><span>View on Google Maps</span></a>');
+        $('.status .loading').remove();
+
 
 
       }
 
     } else {
 
-      $('#status-copy').html('<span>Currently Closed</span>Sorry, The Eggman Truck is closed right now!');
+      $('#status-copy').html('<span>Currently Closed</span>Sorry, we\'re is closed right now!');
+      $('.status .loading').remove();
 
     }
 
@@ -335,37 +316,34 @@ $(".menu_target").on("click", ".close", function() {
 
 
 
-
-
+//
+// Open/Close Contact form
+// 
+//
 
   $("#contact-open-trigger").click(function(e) {
-
     e.preventDefault();
-
     $('body').css('overflow-y', 'hidden');
-
     $('.contact-form-wrap').fadeIn();
-
   });
 
   $("#contact-close").click(function(e) {
-
     e.preventDefault();
-
     $('body').css('overflow-y', 'auto');
-
     $('.contact-form-wrap').fadeOut();
-
   });
 
+
+//
+// Scroll to Schedule
+// 
+//
+
   $('#schedule-trigger').click(function(e) {
-
     e.preventDefault();
-
     $('html, body').animate({
         scrollTop: $(".schedule").offset().top
     }, 700);
-
   });
   
 
